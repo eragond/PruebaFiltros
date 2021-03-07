2103e19dbd5915273bbf0da62e00ca383065884f
Daniel Cruz <eragond@ciencias.unam.mx>
2021-03-07 01:48:10 -0600

    Interfaz funciona, pero le falta mucho orden

--- js/filtro.js
+++ js/filtro.js
@@ -1,18 +1,33 @@
 
-// const EL = (sel) => document.querySelector(sel);
-var canv = null;
-var ctx = null;
-var img = null;
+// Variables globales
+var canvas;                 //Canvas para dibujado.
+var nCanvas;                //Canvas con imagen natural, para procesado.
+var ctx;                    //Contexto de canvas para dibujado.
+var nCtx;                   //Contexto de canvas natural.
+var img;                    //Imagen original.
+var procImg = new Image;    //Imagen para procesar.
 
+
+//Esto se ejecutara, una vez que la pagina haya cargado.
 window.onload = function() {
     //Indicar que cargó.
     console.log("Listo");
 
-    //Cargar canvas e imagen.
-    canv = $("#canv")[0];
-    ctx = canv.getContext("2d");
-    img = $("#imagen")[0];
-    loadCanvasImage();
+    //Inicializar todas las variables.
+    canvas = $("#canv")[0]; //Objeto en la pagina.
+    nCanvas = document.createElement('canvas'); //Objeto natural para procesado.
+    ctx = canvas.getContext("2d"); //Contexto de canvas para pagina.
+    nCtx = nCanvas.getContext("2d"); //Contexto de canvas natural.
+    img = $("#imagen")[0]; //Imagen original.
+
+    //Creamos una copia de la imagen en procImg.
+    nCanvas.width = img.naturalWidth;
+    nCanvas.height = img.naturalHeight;
+    nCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+    guardaImagenADescargar();
+
+    //Carga el preview del canvas de dibujado.
+    cargaPreviewCanvas();
 
     //Cargar tooltips
     $('[data-toggle="tooltip"]').tooltip();
@@ -22,82 +37,73 @@ window.onload = function() {
 // EVENTOS
 // Cambiar tamaño de canvas cuando se actualiza el tamaño de imagen.
 $(window).on("resize", () => {
-    loadCanvasImage();
+    cargaPreviewCanvas();
 });
 
 $("#wrapper").on(
     "toggledImage", () => {
     setTimeout(() => {
-    loadCanvasImage();
+    cargaPreviewCanvas();
 },350);
+});
+
+$("#procesame").click(() => {
+    // console.log("Hola");
+    cargaCanvas();
 });
 
 // Subir imagen
 $("#fileUpload").change(readImage);
 
-
-
-function loadCanvasImage() {
-    canv.width = img.width;
-    canv.height = img.height;
-    var scale = 1;
-    // Escala POR SI SE OCUPA
-    // var scale = Math.max(canv.width / img.width, canv.height / img.height);
-    // Superior izquierda.
-    // var x = (canv.width / 2) - (img.width / 2) * scale;
-    // var y = (canv.height / 2) - (img.height / 2) * scale;
-    ctx.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+// Guarda lo que esta en el canvas natural y lo deja disponible para descargar.
+ function guardaImagenADescargar(redibuja = true) {
+    if (redibuja)
+        procImg.src = nCanvas.toDataURL("image/png");
+    // ctx.drawImage(procImg, 0, 0, procImg.naturalWidth, procImg.naturalHeight);
+    // $("#bajarImagen").attr("download", "img.png");
+    $("#bajarImagen").attr("href", procImg.src);
 }
 
+// Dibuja el preview con el canvas para preview y la imagen generada.
+function cargaPreviewCanvas() {
+    canvas.width = img.width;
+    canvas.height = img.width;
+    ctx.drawImage(procImg, 0, 0, procImg.naturalWidth, procImg.naturalHeight, 0, 0, canvas.width, canvas.height);
+}
+
+// Lee una imagen y la guarda.
 function readImage() {
   if (!this.files || !this.files[0]) return;
 
   const FR = new FileReader();
   FR.addEventListener("load", (evt) => {
-    img.addEventListener("load", () => {
-      loadCanvasImage();
-    });
     img.src = evt.target.result;
+    procImg.src = evt.target.result;
+    img.addEventListener("load", () => {
+        cargaPreviewCanvas();
+        guardaImagenADescargar(false);
+    });
   });
   FR.readAsDataURL(this.files[0]);
 }
 
 function cargaCanvas() {
-    // canv = document.getElementById("canv");
-    // var img = document.getElementById("blah");
-    // img.crossOrigin = "Anonymous";
-    // ctx = canv.getContext("2d");
-    // canv.width = img.width;
-    // canv.height = img.height;
-    // canv.width = 100;
-    // canv.height = 100;
-    ctx.drawImage(img,0,0);
-
-    var imgData = ctx.getImageData(0, 0, canv.width, canv.height);
-    var data = imgData.data;
-
-    grayscale();
-
-    function grayscale() {
-      for (var i = 0; i < data.length; i += 4) {
-        var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-        data[i]     = avg; // red
-        data[i + 1] = avg; // green
-        data[i + 2] = avg; // blue
-      }
-    ctx.putImageData(imgData, 0, 0);
-    }
+    nCanvas.width = procImg.naturalWidth;
+    nCanvas.height = procImg.naturalHeight;
+    nCtx.drawImage(procImg, 0, 0, procImg.naturalWidth, procImg.naturalHeight);
+    var imgData = nCtx.getImageData(0, 0, procImg.naturalWidth, procImg.naturalWidth);
+    grayscale(imgData.data);
+    nCtx.putImageData(imgData, 0, 0);
+    guardaImagenADescargar();
+    cargaPreviewCanvas();
 }
 
-// grayscale = function(ctx) {
-//     var imgData = ctx.getImageData(0, 0, canv.width, canv.height);
-//     var data = imgData.data;
-//     for (var i = 0; i < data.length; i += 4) {
-//       var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-//       data[i]     = avg; // red
-//       data[i + 1] = avg; // green
-//       data[i + 2] = avg; // blue
-//     }
-//   ctx.putImageData(imgData, 0, 0);
-//   }
-// }
+function grayscale(data) {
+  for (var i = 0; i < data.length; i += 4) {
+    var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i]     = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+  }
+  console.log(data.length / 4);
+}
