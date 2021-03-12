56a7f5908cef46fff4ecd785973ea4858eeff319
Daniel Cruz <eragond@ciencias.unam.mx>
2021-03-12 00:38:40 -0600

    Terminados los filtros, falta hacer bonita la doc

--- js/filtro.js
+++ js/filtro.js
@@ -1,16 +1,25 @@
 
 class Filtro {
-    // constructor() {
-    //     this.nombre = "Vacio"
-    // }
-    //
-    procesa(data) {
-        for (var i = 0; i < data.length; i += 4) {
-          this.procPixel(data, i);
-        }
+    constructor(imgw, imgh) {
+        this.nombre = "Vacio";
     }
 
-    // Se ve feo, pero es mucho mas eficiente.
+    setWidthHeight(width, height){
+        this.imgWidth = width;
+        this.imgHeight = height;
+    }
+
+
+    procesa(data) {
+        for (var i = 0; i < data.length; i += 4)
+          this.procPixel(data, i);
+
+        // Otro recorrido
+        // for (let j = 0; j < this.imgHeight; j++)
+        //      for (let i = 0; i < this.imgWidth; i++)
+        //          this.procPixel(data, (i + j*this.imgWidth)*4);
+    }
+
     procPixel(data, i){
         var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
         data[i] = avg;
@@ -99,13 +108,77 @@ export class GrisAzul extends Filtro {
 }
 
 export class Mosaico extends Filtro {
+
     procesa(data) {
-        let width = prompt("Ancho");
-        let height = prompt("Alto");
+        //Obtener informacion sobre las dimensiones del mosaico.
+        let width, height;
+        while (isNaN(width))
+            width = prompt("Escribe un ancho valido", 10);
+        while (isNaN(height))
+            height = prompt("Escribe un alto valido", 10);
+        width = Math.abs(parseInt(width));
+        height = Math.abs(parseInt(height));
+        console.log(width);
+        console.log(height);
+
+        // Procesamiento, recorremos secciones del tamaño indicado.
+        if (!isNaN(width) && !isNaN(height))
+            for (let y = 0; y < this.imgHeight; y += height)
+                for (let x = 0; x < this.imgWidth; x += width)
+                    this.mosaicoSeccion(data, x, y, width, height);
+
+        console.log("termin");
+
     }
+
+    //Obtiene el color promedio de una seccion y lo aplica a cada pixel en esta.
+    mosaicoSeccion(data, x, y, wd, ht){
+        let sumaTotal = [0,0,0];  //Auxiliar para guardar los colores promedio.
+        let total = 0;            //Auxiliar para obtener el total de pixeles en la seccion.
+
+        for (let round = 0; round < 2; round++){    //Dos rondas
+            for (let j = 0; j < ht; j++){           //Recorremos la seccion de anchura x altura.
+                for (let i = 0; i < wd; i++){
+                    let place = ((x+i) + (y+j)*this.imgWidth)*4;    //Posicion del pixel en el arreglo data.
+                    if (i + x < this.imgWidth && j + y < this.imgHeight) { //Verificamos fronteras.
+                        if (round == 0) {           //Primera ronda, leer info de los pixeles.
+                            total += 1;             //Contador para promediar.
+                            sumaTotal[0] += data[place];
+                            sumaTotal[1] += data[place+1];
+                            sumaTotal[2] += data[place+2];
+                        } else {                    //Segunda ronda, escribir sobre los pixeles.
+                            data[place] = sumaTotal[0];
+                            data[place+1] = sumaTotal[1];
+                            data[place+2] = sumaTotal[2];
+                        }
+                    }
+                }
+            }
+            sumaTotal = sumaTotal.map(t => t/total); //Procesar el promedio entre rondas.
+        }
+
+    }
+}
+
+export class Brillo extends Filtro {
+    constructor(){
+        super();
+        this.cte = 0;
+    }
+
+    procesa(data) {
+        //Obtener informacion sobre la constante a utilizar.
+        let ccte;
+        while (isNaN(ccte))
+            ccte = prompt("Escribe un numero valido", 0);
+        this.cte = parseInt(ccte);
+        if (!isNaN(this.cte))
+            super.procesa(data)
+    }
+
     procPixel(data, i){
-        var gris = data[i+2];
-        data[i] = gris;
-        data[i+1] = gris;
+        data[i] += this.cte;
+        data[i+1] += this.cte;
+        data[i+2] += this.cte;
     }
 }
--- js/pintor.js
+++ js/pintor.js
@@ -4,14 +4,14 @@ export default class Pintor {
     constructor(img) {
         this.nombre = "Carlos"                      //El pintor merece identidad.
         this.listaFiltros = {}                      //Creamos la lista de filtros.
-        for (let clase in filtros)                  //Llenamos la lista de filtros.
-            this.listaFiltros[clase] = new filtros[clase]();
         this.canvas = document.createElement('canvas'); //Creamos un canvas y su contexto.
         this.ctx = this.canvas.getContext("2d");
         this.img = img;                                 //Guardamos la imagen.
         this.oImg = new Image();
         this.oImg.src = img.src;
         this.superpon = false;
+        for (let clase in filtros)                  //Llenamos la lista de filtros.
+            this.listaFiltros[clase] = new filtros[clase]();
     }
 
     updateImgSrc(imgSrc) {
@@ -27,6 +27,7 @@ export default class Pintor {
         this.ctx.drawImage(tempImg, 0, 0, this.img.naturalWidth, this.img.naturalHeight);
         let imgData = this.ctx.getImageData(0, 0, this.img.naturalWidth, this.img.naturalWidth);
         // let ini = performance.now()
+        this.listaFiltros[filtro].setWidthHeight(this.img.naturalWidth, this.img.naturalHeight);
         this.listaFiltros[filtro].procesa(imgData.data);
         // let fin = performance.now()
         // console.log("Perfomance global " + (fin - ini));
