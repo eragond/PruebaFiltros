7d6f79aab63a77329580015d7cb8ad26d0dc2fa1
eragond <eragond@ciencias.unam.mx>
2021-07-27 18:26:11 -0500

    Terminado imagenes a puntos

--- docs/BibliotecaImagenes.js
+++ docs/BibliotecaImagenes.js
@@ -9,6 +9,8 @@ export class Biblioteca {
     //El constructor debe generar la biblioteca de filtros
     //De oscuro a mas claro
     constructor() {
+        this.canvas.width = this.swidth*this.ntonos;
+        this.canvas.height = this.sheight;
     }
 
     //Dado un tono de gris (0 - 255) regresa la metadata del tono que corresponde.
@@ -19,41 +21,46 @@ export class Biblioteca {
 }
 
 export class BibliotecaCirculo extends Biblioteca {
-    swidth = 10;         //Ancho de cada imagen de la biblioteca
-    sheight = 10;        //Alto de cada imagen de la biblioteca
+    swidth = 9;         //Ancho de cada imagen de la biblioteca
+    sheight = 9;        //Alto de cada imagen de la biblioteca
     ntonos = 12;        //Numero de tonos en la biblioteca
 
     constructor() {
         super();
         let place = 0;
         for(let i = this.ntonos; i > 0; i -= 1) { 
-            place += this.swidth;
             this.ctx.beginPath();
-            this.ctx.arc(place/2, this.sheight/2, i/4, 0, 2 * Math.PI);
+            this.ctx.arc(place+this.swidth/2, this.sheight/2, i/4, 0, 2 * Math.PI);
             this.ctx.fill();
             this.ctx.stroke();
+            place += this.swidth;
         }
     }
 }
 
 export class BibliotecaPuntos extends Biblioteca {
-    swidth = 10;         //Ancho de cada imagen de la biblioteca
-    sheight = 10;        //Alto de cada imagen de la biblioteca
-    ntonos = 9;        //Numero de tonos en la biblioteca
 
-    constructor() {
+    constructor(puntos, dims, rpunto=1) {
         super();
+        this.swidth = dims;
+        this.sheight = dims;
+        this.ntonos = puntos ** 2;
+        this.canvas.width = this.swidth*this.ntonos;
+        this.canvas.height = this.sheight;
         let place = 0;
-        let pptDim = swidth/3;
+        let pptDim = this.swidth/puntos;
         for(let i = this.ntonos; i > 0; i -= 1) { 
-            place += this.swidth;
             let level = 0;
+            let col = 0;
             for(let np = 0; np < i; np += 1) {
+                level = Math.floor(np/puntos);
+                col = np%puntos;
                 this.ctx.beginPath();
-                this.ctx.arc(place+(pptDim*np), /2, i/4, 0, 2 * Math.PI);
+                this.ctx.arc(place+(pptDim*col)-pptDim/2, level*pptDim+pptDim/2, rpunto, 0, 2 * Math.PI);
                 this.ctx.fill();
                 this.ctx.stroke();
             } 
+            place += this.swidth;
         }
     }
 }
